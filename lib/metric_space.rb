e6949d82c0ecd43d43ef09e59967e7b2f16da3c2
Aleksander Malaszkiewicz <alek@fractalsoft.org>
2013-08-13 14:46:59 +0200

    Clean up

--- lib/metric_space.rb
+++ lib/metric_space.rb
@@ -1,9 +1,6 @@
-require "metric_space/metric"
-require "metric_space/british_rail"
-require "metric_space/euclidean"
-require "metric_space/maximum"
-require "metric_space/taxicab"
-require "metric_space/version"
-
-module MetricSpace
-end
+require 'metric_space/metric'
+require 'metric_space/british_rail'
+require 'metric_space/euclidean'
+require 'metric_space/maximum'
+require 'metric_space/taxicab'
+require 'metric_space/version'
--- spec/metric_space/taxicab_spec.rb
+++ spec/metric_space/taxicab_spec.rb
@@ -1,22 +1,22 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe MetricSpace::Taxicab do
   subject { MetricSpace::Taxicab }
 
   [
     {
-      point1: {a: -0.25, b: 3.25, c: 0.8},
-      point2: {a: 0.8, b: 1.25, c: 0.5},
+      point1: { a: -0.25, b: 3.25, c: 0.8 },
+      point2: { a: 0.8, b: 1.25, c: 0.5 },
       distance: 3.35
     },
     {
-      point1: {a: 0.8, c: 0.5},
-      point2: {b: 0.3, c: 1},
+      point1: { a: 0.8, c: 0.5 },
+      point2: { b: 0.3, c: 1 },
       distance: 1.6
     }
   ].each do |example|
     point1, point2, distance = example.values
-    it "should return #{distance} for distance between #{point1} and #{point2}" do
+    it "is #{distance} between #{point1} and #{point2}" do
       subject.distance(point1, point2).round(5).should eq(distance)
     end
   end
